750fe20354893b9b4f63d86692c95b6258ab1148
yogaandriansyah236 <yogaandriansyah236@gmail.com>
2021-09-14 10:52:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!!!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore suscipit nam mollitia quod aspernatur repellat minus ullam doloremque. Aliquam dolore unde debitis obcaecati? Soluta ipsum ex numquam culpa, iste cum.</p>
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>
 </body>
 </html>
